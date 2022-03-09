2b87ba907d802c6c6605ce32a0b41b2772b1c32a
elias <elias.damisch@gmail.com>
2022-03-09 20:03:54 +0100

    blog freecode

--- Blog-site-freecodecamp/assets/js/main.js
+++ Blog-site-freecodecamp/assets/js/main.js
@@ -19,7 +19,15 @@ const scrollHeader = () => {
 
 window.addEventListener("scroll", scrollHeader);
 // Open menu & search pop-up
+const menuToggleIcon = selectElement("#menu-toggle-icon");
 
+const toggleMenu = () => {
+  const mobileMenu = selectElement("#menu");
+  mobileMenu.classList.toggle("activated");
+  menuToggleIcon.classList.toggle("activated");
+};
+
+menuToggleIcon.addEventListener("click", toggleMenu);
 // Open/Close search form popup
 
 // -- Close the search form popup on ESC keypress
